da7e7507f65477b1447f418010859e15e23551f0
eric_arseneau <eric_arseneau@b37438b9-3d43-0410-a8b3-de56a76a6035>
2009-03-02 01:23:33 +0000

    Created branch java5.

---
+++ branches/java5/cldc/src/java/lang/annotation/Documented.java
@@ -0,0 +1,8 @@
+package java.lang.annotation;
+
+import com.sun.squawk.Java5Marker;
+
+@Java5Marker
+public @Documented @Retention( RetentionPolicy.RUNTIME) @Target( ElementType.ANNOTATION_TYPE ) @interface Documented {
+}
+
