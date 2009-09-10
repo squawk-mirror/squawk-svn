9eb511e057c7ad3fd09397a0ed56cca5aeb6febf
derek_white <derek_white@b37438b9-3d43-0410-a8b3-de56a76a6035>
2009-09-10 21:42:09 +0000

    Added support for specifying Squawk command line arguments in a file "SQUAWK_CMD_LINE". Squawk will startup with or without finding a command line file. Did NOT update the sdk builder to copy the template to the sdk, or the template to the cRIO (yet).

--- branches/squawk-native/vmcore/src/rts/vxworks/os_main.c
+++ branches/squawk-native/vmcore/src/rts/vxworks/os_main.c
@@ -28,41 +28,113 @@
 
 #include <taskLib.h>
 
-#define VXLOADARG(arg) if(arg != NULL) { argv[argc] = arg; argc++; printf("arg: %s\n", arg);} else (void)0
+#define MAXARGS 11
+#define LARGESTARG 64
+
+#define CMD_LINE_FILENAME "SQUAWK_CMD_LINE"
 
 void Priv_SetWriteFileAllowed(int);
+
+int VXLOADARG(char* arg, char** argv, int argc) {
+    if (arg != NULL) {              
+        if (argc >= MAXARGS) {       
+            fprintf(stderr, "too many args, skipping: %s\n", arg); 
+        } else {                     
+            argv[argc] = arg;        
+            argc++;                  
+            printf("arg: %s\n", arg);
+        }
+    }
+    return argc;
+}
+
+/**
+ * Parse input from cmdFile into whitespace-seperated argument strings.
+ * Comment lines begin with #
+ *
+ * @return fresh string or null if no more arguments.
+ */
+char* getnextArg(FILE* cmdFile) {
+    int ch;
+    int index = 0;
+    char argbuf[LARGESTARG+1];
+
+    ch = fgetc(cmdFile);
+    while (true) {
+        if (ch == '#') {
+            while ((ch = fgetc(cmdFile)) != '\n') {
+                // strip until end-of-line
+            }
+            ch = fgetc(cmdFile); // skip over \n
+        } else if (ch == EOF || isspace(ch)) {
+            if (index > 0) {
+                // process arg
+                char* copyarg = malloc(index + 1);
+                argbuf[index] = '\0';
+                strcpy(copyarg, argbuf);
+                return copyarg;
+            } else if (ch == EOF) {
+                return NULL;
+            }
+            while (isspace(ch = fgetc(cmdFile))) {
+                // strip extra spaces... terminate on EOF or non-space
+            }
+        } else {
+            if (index >= LARGESTARG - 1) {
+                argbuf[LARGESTARG - 1] = '\0';
+                fprintf(stderr, "Argument too long: %s\n", argbuf);
+                return NULL;
+            }
+            // buffer up argument until we see EOF or space:
+            argbuf[index++] = ch;
+            ch = fgetc(cmdFile);
+        }
+    }
+}
 
 /**
  * Entry point for the VxWorks operating system.
  */
-int os_main(char* arg1, char* arg2, char* arg3, char* arg4, char* arg5, char* arg6, char* arg7, char* arg8, char* arg9, char* arg10) {
+int os_main(char* arg1, char* arg2, char* arg3, char* arg4, char* arg5) {
     // Convert from VxWorks argument format to normal argument format
 
-    char* argv[11];
-    int argc = 1;
+    char* argv[MAXARGS];
+    int argc = 0;
+    FILE* cmdFile;
 
-    argv[0] = "squawk.out";
+    argc = VXLOADARG("squawk.out", argv, argc);
 
-    VXLOADARG(arg1);
-    VXLOADARG(arg2);
-    VXLOADARG(arg3);
-    VXLOADARG(arg4);
-    VXLOADARG(arg5);
-    VXLOADARG(arg6);
-    VXLOADARG(arg7);
-    VXLOADARG(arg8);
-    VXLOADARG(arg9);
-    VXLOADARG(arg10);
+    // load cmd line args from file
+    cmdFile = fopen(CMD_LINE_FILENAME, "r");
+    if (cmdFile != NULL) {
+        fprintf(stderr, "Reading Squawk command line file " CMD_LINE_FILENAME "...\n");
+        char* copyarg;
+        while ((copyarg = getnextArg(cmdFile)) != NULL) {
+            argc = VXLOADARG(copyarg, argv, argc);
+        }
+        fclose(cmdFile);
+    } else {
+        fprintf(stderr, "Squawk command line file " CMD_LINE_FILENAME " not found, using default args...\n");
+        argc = VXLOADARG("-suite:robot", argv, argc);
+        argc = VXLOADARG("-Xmxnvm:1M",   argv, argc);
+    }
+
+    // load programtic args (debugger commands, etc):
+    argc = VXLOADARG(arg1, argv, argc);
+    argc = VXLOADARG(arg2, argv, argc);
+    argc = VXLOADARG(arg3, argv, argc);
+    argc = VXLOADARG(arg4, argv, argc);
+    argc = VXLOADARG(arg5, argv, argc);
 
     return Squawk_main_wrapper(argc, argv);
 }
 
 void robotTask() {
-    os_main("-suite:robot", "-verbose", "-Xtgc:1", "-Xmxnvm:1M", null, null, null, null, null, null);
+    os_main(null, null, null, null, null);
 }
 
 void robotTask_DEBUG() {
-    os_main("-suite:robot", "-verbose", "-Xtgc:1",  "-Xmxnvm:1M", "com.sun.squawk.debugger.sda.SDA", "com.sun.squawk.imp.MIDletMainWrapper", "MIDlet-1", null, null, null);
+    os_main("com.sun.squawk.debugger.sda.SDA", "com.sun.squawk.imp.MIDletMainWrapper", "MIDlet-1", null, null);
 }
 
 /**
@@ -78,12 +150,12 @@ int squawk_StartupLibraryInit(char* arg1, char* arg2, char* arg3, char* arg4, ch
 
     fd = open("SQUAWK_DEBUG_ENABLED", O_RDONLY);
     if (fd >= 0) {
-        fprintf(stderr, "File SQUAWK_DEBUG_ENABLED found, starting squawk in debug mode...");
+        fprintf(stderr, "File SQUAWK_DEBUG_ENABLED found, starting squawk in debug mode...\n");
         entryPt = (FUNCPTR)robotTask_DEBUG;
         close(fd);
         remove("SQUAWK_DEBUG_ENABLED");
     } else {
-        fprintf(stderr, "File SQUAWK_DEBUG_ENABLED not found, starting squawk in normal mode...");
+        fprintf(stderr, "File SQUAWK_DEBUG_ENABLED not found, starting squawk in normal mode...\n");
     }
 
     /*
