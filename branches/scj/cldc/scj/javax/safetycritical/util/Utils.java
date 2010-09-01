4a07beb286645f72067ac64b0af8b4c4523b5b23
zhao54 <zhao54@b37438b9-3d43-0410-a8b3-de56a76a6035>
2010-09-01 21:41:16 +0000

    minor

--- branches/scj/cldc/scj/javax/safetycritical/util/Utils.java
+++ branches/scj/cldc/scj/javax/safetycritical/util/Utils.java
@@ -3,4 +3,8 @@ package javax.safetycritical.util;
 public class Utils {
 
     public static boolean DEBUG = false;
+
+    public static void unimplemented() {
+        throw new MethodUnimplementedException();
+    }
 }
